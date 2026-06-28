a871db1852c2c2b8d4e581a994f2072054d7d5ad
Abdoul-Aziz-Ndour <abdoulazizndour2704@gmail.com>
2026-06-28 18:34:07 +0000

    fix: ajout URL google dans urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import RegisterView, LoginView, LogoutView, ProfileView, MeView
+from .google_auth import GoogleLoginView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -8,7 +9,6 @@ urlpatterns = [
     path('logout/',   LogoutView.as_view(),   name='logout'),
     path('refresh/',  TokenRefreshView.as_view(), name='token_refresh'),
     path('me/',       MeView.as_view(),       name='me'),
-
-    # Profil public : /api/auth/users/<id>/
+    path('google/',   GoogleLoginView.as_view(), name='google-login'),
     path('users/<int:pk>/', ProfileView.as_view(), name='user-profile'),
 ]
